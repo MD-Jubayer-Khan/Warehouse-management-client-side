0d383fb916aed1a7b0250393260c98de220a420d
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-06-25 16:21:23 +0600

    create some component

---
+++ src/component/Home/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../../shared/Footer/Footer';
+import Header from '../../shared/Header/Header';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Banner></Banner>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/component/Home/InventoryItems/InventoryItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InventoryItems = () => {
+    return (
+        <div>
+            <h1>This is Inventory items</h1>
+        </div>
+    );
+};
+
+export default InventoryItems;
